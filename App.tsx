15d4320ad3f10caa0e6a2956e770260a8c9067d9
Mokshi Gupta <mokshigupta0281@gmail.com>
2021-11-16 21:03:22 +0530

    Created Expo App

--- App.tsx
+++ App.tsx
@@ -1,25 +1,76 @@
-//import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import React, { Component, useState } from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
-export default function App() {
-  return (
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={{backgroundColor: '#aed'}}>Welcome To My App!</Text>
+//       <Text style={styles.myBackground}>Proceed Further</Text>
+//       <Text style={styles.myBackground}>Proceed</Text>
+//     </View>
+//   );
+// }
+
+function HomeScreen({navigation}){
+  return(
     <View style={styles.container}>
-      <Text style={{backgroundColor: '#aed'}}>Welcome To My App!</Text>
-      <Text style={styles.myBackground}>Proceed Further</Text>
-      <Text style={styles.myBackground}>Proceed</Text>
+      <StatusBar style="auto"/>
+      <Text style={styles.textStyle}>This is a Home Screen</Text>
+      <Button title="Profile Page"
+      onPress = {
+        //()=> navigation.navigate("Profile")
+        ()=> navigation.push("Profile")
+      }
+      ></Button>
     </View>
   );
+}
+
+function ProfileScreen(){
+  return(
+    <View style={styles.container}>
+      <Text style={styles.textStyle}>This is a Profile Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App(){
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{title:"Home Screen"}}/>
+        <Stack.Screen name="Profile" component={ProfileScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    fontSize: 24,
+    color: "#f00",
+    marginBottom: 12
+  },
   myBackground: {
-    backgroundColor: '#ffaaee',
-    margin: 18
-  }
+    backgroundColor: '#fae',
+    fontSize: 24,
+    marginBottom: 20
+  },
+  image: {
+    width: 300,
+    height: 200,
+    marginBottom: 20
+  }, 
 });
+
---
+++ src/tutorials/ClassComponet.tsx
@@ -0,0 +1,60 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component, useState} from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
+export default class App extends Component{
+    idx = 0;
+    quotes = ["Be Exceptional", "Work Hard, Be Successfill",
+    "Search the candle rather than cursing the darkness"];
+
+    state = {
+        quote: this.quotes[this.idx]
+    }
+
+    onButtonPressed = () => {
+        this.idx++;
+        if(this.idx == 2){
+            this.idx = 0;
+        }
+
+        this.setState(
+            {
+                quote: this.quotes[this.idx]
+            }
+        );
+    }
+
+    render(){
+        return(
+            <view style={styles.container}>
+                <StatusBar style = "auto"/>
+                <Text style={styles.textStyle}>{this.state.quote}</Text>
+                <Button title="Next Quote" onPress = {this.onButtonPressed}></Button>
+            </view>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    textStyle: {
+      fontSize: 24,
+      color: "#f00",
+      marginBottom: 12
+    },
+    myBackground: {
+      backgroundColor: '#fae',
+      fontSize: 24,
+      marginBottom: 20
+    },
+    image: {
+      width: 300,
+      height: 200,
+      marginBottom: 20
+    }, 
+  });
